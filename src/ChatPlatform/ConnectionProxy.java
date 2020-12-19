760e4ee91a1cf587dfd7fc6e0ab910441ade6844
Sagi <chuboksagi@gmail.com>
2020-12-19 17:28:21 +0200

    addConsumer

--- src/ChatPlatform/ConnectionProxy.java
+++ src/ChatPlatform/ConnectionProxy.java
@@ -1,7 +1,18 @@
 package ChatPlatform;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
 public class ConnectionProxy extends Thread implements StringProducer,StringConsumer{
+
+    private StringConsumer consumer = null;
+
     @Override
     public void addConsumer(StringConsumer sc) {
+        consumer = sc;
     }
     @Override
     public void removeConsumer(StringConsumer sc) {
