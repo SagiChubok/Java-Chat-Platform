f1a33a6ec9b5e318f94b7fea0a55f43fc27c5be7
LinoyChubok <linoychubok@gmail.com>
2020-12-20 23:35:55 +0200

    start GUI

--- src/ChatPlatform/ClientGUI.java
+++ src/ChatPlatform/ClientGUI.java
@@ -4,14 +4,17 @@ import java.io.*;
 import java.net.*;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
+public class ClientGUI implements StringConsumer {
 
-public class ClientGUI implements StringConsumer, StringProducer {
-    @Override
-    public void addConsumer(StringConsumer sc) {}
-
-    @Override
-    public void removeConsumer(StringConsumer sc) {}
+    private JFrame chatBoard;
+    private JPanel chat_panel;
+    private JPanel send_panel;
+    private JTextArea chatArea;
+    private JTextField text_field;
+    private JButton connectBtn;
+    private JButton sendBtn;
 
     @Override
     public synchronized void consume(String str) {}
@@ -23,7 +26,46 @@ public class ClientGUI implements StringConsumer, StringProducer {
     }
 
     public void go() {
+        /* Chat GUI structure */
+
+        // Layout
+        chatBoard = new JFrame("Chat Platform");
+        chatBoard.setLayout(new BorderLayout());
+        chatBoard.setSize(530, 500);
+
+        chat_panel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        send_panel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+
+        // Layout elements
+        chatArea = new JTextArea("Java Course Shenkar (Chat Platform Assignment)");
+        chatArea.setEditable(false);
+        chatArea.setPreferredSize(new Dimension(503, 400));
+
+        text_field = new JTextField(" Enter your name...");
+        text_field.setPreferredSize(new Dimension(400, 40));
+
+        connectBtn = new JButton("Connect");
+        connectBtn.setPreferredSize(new Dimension(100, 40));
+        connectBtn.addActionListener(new ConnectActionListener(this));
+        connectBtn.setActionCommand("Connect");
+
+        sendBtn = new JButton("Send");
+        sendBtn.setPreferredSize(new Dimension(100, 40));
+        sendBtn.addActionListener(new ConnectActionListener(this));
+        sendBtn.setActionCommand("Send");
+        sendBtn.setVisible(false);
+
+        JScrollPane scroller = new JScrollPane(this.chatArea);
 
     }
+
+    //Actions listeners
+    class ConnectActionListener implements ActionListener {
+
+        public ConnectActionListener(ClientGUI gui){}
+
+        public void actionPerformed(ActionEvent ae) {}
+    }
+
 
 }
