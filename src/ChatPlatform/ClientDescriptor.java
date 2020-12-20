e43694d478c45a6d94b121a912dc91703fc3fc26
Sagi <chuboksagi@gmail.com>
2020-12-20 14:09:55 +0200

    Add class members ClientDescriptor

--- src/ChatPlatform/ClientDescriptor.java
+++ src/ChatPlatform/ClientDescriptor.java
@@ -1,9 +1,13 @@
 package ChatPlatform;
 
-import java.io.IOException;
-
 public class ClientDescriptor implements StringConsumer, StringProducer {
-    public void addConsumer(StringConsumer sc) {}
+
+    private StringConsumer consumer = null;
+    private String name;
+
+    public void addConsumer(StringConsumer sc) { consumer = sc; }
+
     public void removeConsumer(StringConsumer sc) {}
+
     public void consume(String str) {}
 }
