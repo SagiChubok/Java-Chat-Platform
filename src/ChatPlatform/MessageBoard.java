42d1a136eda4c937c65b856a2f8ee912d122637f
LinoyChubok <linoychubok@gmail.com>
2020-12-20 16:19:42 +0200

    addConsumer

--- src/ChatPlatform/MessageBoard.java
+++ src/ChatPlatform/MessageBoard.java
@@ -11,7 +11,9 @@ public class MessageBoard implements StringProducer,StringConsumer {
     }
 
     @Override
-    public void addConsumer(StringConsumer sc) {}
+    public void addConsumer(StringConsumer sc) {
+        listCP.add((ConnectionProxy)sc);
+    }
 
     @Override
     public void removeConsumer(StringConsumer sc) {
