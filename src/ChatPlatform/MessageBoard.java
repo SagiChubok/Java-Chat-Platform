18eb595e4e10f278ef0b6793b554598edf1d0849
LinoyChubok <linoychubok@gmail.com>
2020-12-20 16:15:32 +0200

    add consume to MessageBoard

--- src/ChatPlatform/MessageBoard.java
+++ src/ChatPlatform/MessageBoard.java
@@ -3,17 +3,31 @@ package ChatPlatform;
 import java.util.LinkedList;
 
 public class MessageBoard implements StringProducer,StringConsumer {
-    private LinkedList<StringConsumer> list = null;
+
+    private LinkedList<ConnectionProxy> listCP = null;
 
     MessageBoard() {
-        list = new LinkedList<StringConsumer>();
+        listCP = new LinkedList<ConnectionProxy>();
     }
 
     @Override
     public void addConsumer(StringConsumer sc) {}
+
     @Override
     public void removeConsumer(StringConsumer sc) {
     }
+
     @Override
-    public synchronized void consume(String str) {}
+    public synchronized void consume(String str) {
+        int listSize = listCP.size();
+        for(int i = 0 ; i < listSize ; i++){
+            ConnectionProxy consumer = listCP.get(i);
+            if(consumer != null) {
+                if (consumer.socketExist()) {
+                    consumer.consume(str);
+                }
+                else consumer = null;
+            }
+        }
+    }
 }
