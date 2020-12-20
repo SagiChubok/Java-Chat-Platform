70920270ab48fff855b995201bddebcce50192b3
LinoyChubok <linoychubok@gmail.com>
2020-12-20 14:03:07 +0200

    edit ClientDescriptor

--- src/ChatPlatform/ClientDescriptor.java
+++ src/ChatPlatform/ClientDescriptor.java
@@ -1,4 +1,9 @@
 package ChatPlatform;
 
-public class ClientDescriptor {
+import java.io.IOException;
+
+public class ClientDescriptor implements StringConsumer, StringProducer {
+    public void addConsumer(StringConsumer sc) {}
+    public void removeConsumer(StringConsumer sc) {}
+    public void consume(String str) {}
 }
